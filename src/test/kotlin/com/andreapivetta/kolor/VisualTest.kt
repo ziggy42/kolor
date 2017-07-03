3784c6a7bd636597a67266ae19e46f6ad89dd4c8
Matthew Dean <deanmatthew16@gmail.com>
2017-07-02 20:00:11 -0400

    Add very basic tests

---
+++ src/test/kotlin/com/andreapivetta/kolor/VisualTest.kt
@@ -0,0 +1,15 @@
+package com.andreapivetta.kolor
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.it
+
+object VisualTest : Spek({
+    // This test doesn't make any assertions, but rather lets the tester visually verify that everything is working as
+    // expected
+    it("should print out a string for every color") {
+        for (c in Color.values()) {
+            println(Kolor.foreground("${c.name} in foreground", c))
+            println(Kolor.background("${c.name} in background", c))
+        }
+    }
+})
