5fc4c048debae7c01daf4272c88936e782cffb8c
andrea <vanpivix@gmail.com>
2017-11-19 09:36:08 +0100

    Replace for with forEach

--- src/test/kotlin/com/andreapivetta/kolor/KolorTest.kt
+++ src/test/kotlin/com/andreapivetta/kolor/KolorTest.kt
@@ -1,16 +1,15 @@
 package com.andreapivetta.kolor
 
+import com.andreapivetta.kolor.Kolor.RESET
 import com.winterbe.expekt.should
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 
-import com.andreapivetta.kolor.Kolor.RESET
-
 object KolorTest : Spek({
     describe("foreground/background") {
         it("should create a string that starts with an ANSI code and ends with the reset code") {
-            for (color in Color.values()) {
+            Color.values().forEach { color ->
                 Kolor.foreground("foo", color).should.equal("${color.foreground}foo$RESET")
                 Kolor.background("foo", color).should.equal("${color.background}foo$RESET")
             }
