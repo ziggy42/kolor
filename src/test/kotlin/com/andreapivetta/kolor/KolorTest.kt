d7a8ad8c8fac1f89c0f470390b6e6c99bd2950fd
Andrea Pivetta <vanpivix@gmail.com>
2020-02-17 22:50:06 +0100

    Break tests on purpose

--- src/test/kotlin/com/andreapivetta/kolor/KolorTest.kt
+++ src/test/kotlin/com/andreapivetta/kolor/KolorTest.kt
@@ -2,12 +2,14 @@ package com.andreapivetta.kolor
 
 import com.andreapivetta.kolor.Kolor.RESET
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class KolorTest {
 
     @Test
     fun `should create a string that starts with an ANSI code and ends with the reset code`() {
+        assertTrue(false)
         Color.values().forEach { color ->
             assertEquals("${color.foreground}foo$RESET", Kolor.foreground("foo", color))
             assertEquals("${color.background}foo$RESET", Kolor.background("foo", color))
